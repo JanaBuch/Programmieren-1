7ab6c0240b15117898d2c039b00c301b15c47984
JanaBuch <jana.buchmann@hfg-gmuend.de>
2023-04-11 15:01:29 +0200

    rabbit

---
+++ Symbol/rabbit.js
@@ -0,0 +1,51 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+    
+  
+    noFill();
+  beginShape();
+  strokeWeight(4);
+  stroke(51);
+  curveVertex(200, 100);
+  curveVertex(200, 100);
+  curveVertex(270, 200);
+  curveVertex(280, 280);
+  curveVertex(230, 300);
+  curveVertex(180, 290);
+  curveVertex(150, 230);
+  curveVertex(150, 140);
+  curveVertex(150, 140);
+  endShape();
+  ellipse(162, 110, 80, 60)
+  circle(302, 270, 40)
+  noFill();
+  beginShape();
+  curveVertex(200, 100);
+  curveVertex(200, 100);
+     curveVertex(240, 60);
+  curveVertex(250, 40);
+    curveVertex(240, 30);
+  curveVertex(210, 40);
+  curveVertex(175, 80);
+  curveVertex(140, 40);
+  endShape();
+    
+    noFill();
+  beginShape();
+  curveVertex(230, 300);
+  curveVertex(230, 300);
+  curveVertex(150, 300);
+  curveVertex(150, 280);
+  curveVertex(165, 270);
+  curveVertex(170, 270);
+  endShape();
+    
+  circle(164, 106, 15)
+    
+  
+  
+  }
