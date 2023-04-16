93b3875fad4571209ab7a1353e85616d03329791
JanaBuch <jana.buchmann@hfg-gmuend.de>
2023-04-16 15:25:39 +0200

    finish programm

--- Hausaufgabe1/getelementid.js
+++ Hausaufgabe1/getelementid.js
@@ -1,7 +1,22 @@
-function changeColor(newColor) {
-    const elem = document.getElementById("para");
-    elem.style.color = newColor;
-  }
+let toadColor = "blue"; // Variable für die Textfarbe definieren
+let marioColor = "red";
+let louigiColor = "green";
+let peachColor = "pink";
 
-  let
-  
+// Funktionen für die Button-Clicks definieren
+function greetToad() {
+  document.getElementById('textfeld_id').value = 'Hallo Toad!';
+  document.getElementById('textfeld_id').style.color = toadColor; // Farbe setzen
+}
+function greetMario() {
+  document.getElementById('textfeld_id').value = 'Hallo Mario!';
+  document.getElementById('textfeld_id').style.color = marioColor; // Farbe setzen
+}
+function greetLuigi() {
+  document.getElementById('textfeld_id').value = 'Hallo Luigi!';
+  document.getElementById('textfeld_id').style.color = louigiColor; // Farbe setzen
+}
+function greetPeach() {
+  document.getElementById('textfeld_id').value = 'Hallo Peach!';
+  document.getElementById('textfeld_id').style.color = peachColor; // Farbe setzen
+}
