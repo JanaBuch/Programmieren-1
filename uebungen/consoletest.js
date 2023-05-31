7d37a90c3f63aed3592325ba8b80431868398c34
JanaBuch <128695587+JanaBuch@users.noreply.github.com>
2023-05-31 09:10:18 +0200

    Add Card designs

---
+++ uebungen/consoletest.js
@@ -0,0 +1,12 @@
+const msg = [4, 5, 6]
+
+console.time(); //gibt Zeit aus die für den Vorgang benötight wird
+console.warn('content'); //gibt content als Warnung in der Konsole aus
+console.error('content'); //gibt content als Fehler in der Konsole aus
+
+for (let i = 0; i < 100; i++) {
+    console.log(i)
+}
+
+console.log("hello World")
+console.table(msg) //gibt Tabelle aus
