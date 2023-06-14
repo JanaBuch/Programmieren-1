d875f4256cbc57bb2a78bdccffc937b0544224f6
JanaBuch <128695587+JanaBuch@users.noreply.github.com>
2023-06-14 20:30:58 +0200

    Designs + Reset
    Addded more custom designs for the cards.
    
    Added a reset button for the game

--- FinalProjectMemory/MakeItWork.js
+++ FinalProjectMemory/MakeItWork.js
@@ -4,7 +4,13 @@ let hasFlippedCard = false;
 let firstCard, secondCard;
 let player = 1;
 
-//change player (WIP)
+let seconds = 0;
+let timer;
+
+//Karten erstellen funktion
+
+
+//change player (WIP), Der Spieler wechselt wenn eine runde vorbei ist
 function switchPlayer() {
   if (player = 1) {
     player = 2;
@@ -16,7 +22,21 @@ function switchPlayer() {
   }
 }
 
+//wählt den die Textbox für den Aktiven Spiler aus
 document.getElementById('ActivePlayerSpace').value = player;
+
+function startTimer() {
+  timer = setInterval(updateTimer, 1000); // Timer wird jede Sekunde aktualisiert
+}
+
+function updateTimer() {
+  seconds++;
+  printTime //timer wert in enstpechendes Feld printen
+}
+
+function printTime() {
+  Timer.value = seconds;
+}
 
 //Flip thy cards
 function flipCard() {
@@ -36,7 +56,7 @@ function flipCard() {
   }
 
   secondCard = this;
-  
+
   checkForMatch();
 }
 
@@ -44,7 +64,7 @@ function flipCard() {
 //disable the cards when a match has been found
 function checkForMatch() {
 
-  if (firstCard.dataset.framework === secondCard.dataset.framework) {
+  if (firstCard.dataset.framework === secondCard.dataset.framework) { //Wenn die Karten gleich sind werden sie Aufgedeckt gelassen
 
     disableCards();
 
@@ -57,8 +77,8 @@ function checkForMatch() {
   //reset the cards when no match is found
   else {
     lockBoard = true;
-    //kenzeichnungsfunktion hier hinzufügen
-    setTimeout(() => {
+    //kenzeichnungsfunktion für missmatch hier hinzufügen
+    setTimeout(() => { //Karten bleiben für einen Moment aufgedeckt, und drehen sich dann wieder
       firstCard.classList.remove('flip');
       secondCard.classList.remove('flip');
       resetBoard();
@@ -67,18 +87,18 @@ function checkForMatch() {
 
 }
 
-function disableCards() {
+function disableCards() { //wird usgeführt sobald ein mach gefunden wurde
 
-  firstCard.removeEventListener('click', flipCard);
+  firstCard.removeEventListener('click', flipCard); //der eventlistener wird entfernt so das die karte nich mehr auf ein click reagiert
 
   secondCard.removeEventListener('click', flipCard);
 
-  firstCard.classList.add('match');
+  firstCard.classList.add('match'); //Fügt ein eine CSS Klasse hinzu um den Karten einen Leuchteffekt zu geben
   secondCard.classList.add('match');
 
 }
 
-function resetBoard() {
+function resetBoard() { //Ende der Runde, Zustand das Karten geklickt wurde wird Zurückgesetzt.
 
   [hasFlippedCard, lockBoard] = [false, false];
 
@@ -86,16 +106,11 @@ function resetBoard() {
 
 }
 
-// function markMatch {
-// setTimeout(()=>{
-//   firstCard.classList.add('match');
-//   secondCard.classList.add('match');}, 500)}
-
 (function shuffle() {
 
   cards.forEach(card => {
 
-    let ramdomPos = Math.floor(Math.random() * 12);
+    let ramdomPos = Math.floor(Math.random() * 12); //Karten bekommmen eine Zufällige position zugewiesen
 
     card.style.order = ramdomPos;
 
@@ -104,4 +119,35 @@ function resetBoard() {
 })();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+function resetGame() { //Setzt das Spiel zurück
+  hasFlippedCard = false;
+  lockBoard = false;
+  firstCard = null;
+  secondCard = null;
+
+  // Zurücksetzen der Karten
+  cards.forEach(card => {
+    card.classList.remove('flip');
+    card.classList.remove('match');
+    card.addEventListener('click', flipCard);
+  });
+
+  // Karten neu mischen
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+
+  //add reset timer
+}
+
+// function markMatch { //Fehlgeschlagerner versuch den Karten erst nach einer kurzen Zeit einen Leuchteffekt
+// setTimeout(()=>{
+//   firstCard.classList.add('match');
+//   secondCard.classList.add('match');}, 500)}
+
+
+//if alle karten === klasse match, timer stoppen, sieger anzeigen, game decided
+
 
